f1c1d71fc505afd32aba0e517291c470a9da55e0
nizam <nizamchavarri@gmail.com>
2025-11-28 11:54:46 +0100

    FEAT: Clase Usuarios creada

--- src/main/java/es/potersitos/modelos/Usuarios.java
+++ src/main/java/es/potersitos/modelos/Usuarios.java
@@ -1,17 +1,77 @@
 package es.potersitos.modelos;
 
-import es.potersitos.controladores.ControladorVentana;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.ResourceBundle;
-
+/**
+ * Clase que representa el modelo de datos de un Wizard (Mago/Usuario).
+ */
 public class Usuarios {
 
-    /** Logger para esta clase */
-    private static final Logger logger = LoggerFactory.getLogger(Usuarios.class);
+    private String id;
+    private String name;
+    private String house;
+    private String wand;
+    private String imageUrl;
 
-    /** Bundle del sistema de internacionalización */
-    private ResourceBundle bundle;
+    public Usuarios() {
+    }
 
+    public Usuarios(String id, String name, String house, String wand, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.house = house;
+        this.wand = wand;
+        this.imageUrl = imageUrl;
+    }
+
+    // --- Getters y Setters ---
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+    public String getWand() {
+        return wand;
+    }
+
+    public void setWand(String wand) {
+        this.wand = wand;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Wizard{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", house='" + house + '\'' +
+                ", wand='" + wand + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
 }
